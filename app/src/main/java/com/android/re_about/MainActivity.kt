5b0ae85673162f4d970b307e85a1bba73c0e7054
Jun Beom Park <rheo191710@gmail.com>
2023-10-22 23:52:51 +0900

    .

--- app/src/main/java/com/android/re_about/MainActivity.kt
+++ app/src/main/java/com/android/re_about/MainActivity.kt
@@ -2,10 +2,23 @@ package com.android.re_about
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.TextView
+import androidx.constraintlayout.widget.ConstraintLayout
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val tv_id = findViewById<TextView>(R.id.tv_id)
+
+        if(intent.hasExtra("id")) {
+            tv_id.text = "아이디 : "+ intent.getStringExtra("id")
+        }
+
+        val btn_close = findViewById<ConstraintLayout>(R.id.btn_close)
+        btn_close.setOnClickListener{
+            finish()
+        }
     }
 }
